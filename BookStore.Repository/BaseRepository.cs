3d72c21fc2d25af5347aac733487a34e45229510
Sahil Nasariwala <sahilnasariwala123@gmail.com>
2022-06-22 15:25:02 +0530

    Initial Commit.

---
+++ BookStore.Repository/BaseRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Models.ViewModels;
+
+namespace BookStore.Repository
+{
+    public class BaseRepository
+    {
+        protected readonly BookStore1Context _context = new BookStore1Context();
+    }
+}
